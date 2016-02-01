c075a2bea8abbee8247cb08544fce1de9ae6da89
DAvide Colla <dado150@gmail.com>
2016-02-01 19:47:51 +0100

    Initial Commit

---
+++ ShakeTrailer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShakeTrailer
+//
+//  Created by DAdo150 on 2/1/16.
+//  Copyright © 2016 DAdo150. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
